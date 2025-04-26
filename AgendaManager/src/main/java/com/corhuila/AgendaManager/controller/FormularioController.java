47f8f31497ad5d79699a04238938ab05e95dff4b
arcelot27 <arctamua@gmail.com>
2025-04-25 19:03:34 -0500

    ✨  feature [H3]: Generate  funcionality

--- AgendaManager/src/main/java/com/corhuila/AgendaManager/controller/FormularioController.java
+++ AgendaManager/src/main/java/com/corhuila/AgendaManager/controller/FormularioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/formulario")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FormularioController {
 
     @Autowired
